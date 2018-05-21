0f197666f60a6115c85f132d8ee61752dca779c0
Sergey <sergeygonchar88@gmail.com>
2018-05-22 01:38:26 +0300

    1.0

--- js/main.js
+++ js/main.js
@@ -1,40 +1,71 @@
 var getList = document.querySelector('#getList'),
-    resultList = [];
+    flag = true;
 
 getList.addEventListener('click', function () {
-    startReq('json/data1.json');
-    startReq('json/data2.json');
+    resultList = [];
+    startReq('json/data1.json', 'json/data2.json');
 })
 
-function startReq(url) {
+function startReq(url1, url2) {
+    httpRequest(url1)
+        .then(
+            function () {
+                return httpRequest(url2)
+            }, function () {
+                console.log('error')
+            }
+        )
+        .then(
+            function () {
+                localStorage.setItem('resultList', JSON.stringify(resultList));
+                condition();
+            }, function () {
+                console.log('error')
+            }
+        );
+}
 
-    var promise = new Promise(function (resolve, reject) {
-
+function httpRequest(url) {
+    return new Promise(function (resolve, reject) {
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState != 4) {
-                return;
-            }
-
-            if (xhr.status != 200) {
-                reject();
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                resultList = resultList.concat(JSON.parse(xhr.response))
+                resolve();
             } else {
-                resolve(xhr.responseText);
+                reject();
             }
-        }
-
+        };
+        xhr.onerror = function () {
+            reject();
+        };
         xhr.send();
-
-    })
-
-    promise
-        .then(function (response) {
-            resultList = resultList.concat(JSON.parse(response));
-        }, function () {
-            console.log('Server is not responding');
-        })
+    });
 }
+
+function condition() {
+    if (localStorage['resultList'] && flag) {
+        render();
+    }
+}
+
+function render() {
+    var renderList = JSON.parse(localStorage.getItem('resultList'));
+    renderList.forEach(function (item) {
+        newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = item;
+        document.body.appendChild(newDiv);
+    })
+    flag = !flag;
+}
+
+window.onload = function () {
+    if (localStorage['resultList']) {
+        render();
+    }
+}
+
 
 
